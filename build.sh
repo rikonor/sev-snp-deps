e6e967434ce6523985d671e7618cb7afb8404a56
Or Ricon <rikonor@gmail.com>
2023-10-26 13:26:47 -0400

    build

--- build.sh
+++ build.sh
@@ -2,4 +2,21 @@
 
 set -euo pipefail
 
-echo "Building Linux Kernel ${LOCALVERSION}"
+OUTPUT_DIR=$(mktemp -d)
+CONFIG_CMD="scripts/config --file ${OUTPUT_DIR}/.config"
+
+CONFIG_PATH="${CONFIG_PATH:-}"
+if [[ -z "${CONFIG_PATH}" ]]; then
+    echo "missing CONFIG_PATH"
+    exit 1
+fi
+
+# Clean up
+make O=${OUTPUT_DIR} distclean
+
+# Configuration
+cp "${CONFIG_PATH}" "${OUTPUT_DIR}"
+${CONFIG_CMD} --set-str LOCALVERSION "${LOCALVERSION}"
+
+# Build and package
+make O=${OUTPUT_DIR} -j$(nproc) bindeb-pkg
