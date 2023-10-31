ae21563e10286516484e3ee75d76129b97c24b2c
Or Ricon <rikonor@gmail.com>
2023-10-31 13:51:30 -0400

    more

--- build.sh
+++ build.sh
@@ -2,4 +2,24 @@
 
 set -euo pipefail
 
-echo "Building QEMU"
+SOURCE_DIR="${SOURCE_DIR:-}"
+if [[ -z "${SOURCE_DIR}" ]]; then
+    echo "missing SOURCE_DIR"
+    exit 1
+fi
+
+# Configure
+${SOURCE_DIR}/configure \
+    --target-list=x86_64-softmmu \
+    --with-git-submodules=ignore
+
+# Build
+make -j$(nproc)
+
+# Install
+make install
+
+# Package
+checkinstall -D -y \
+    --pkgname=qemu \
+    --pkgversion=8
